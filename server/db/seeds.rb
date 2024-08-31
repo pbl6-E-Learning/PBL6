7cb86b5abe5f80aafe3f9aac54497240772fcc5a
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-08-31 18:01:15 +0700

    SCRUM-55 <seed data user>

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Account.create!(
+  email: "admin@mail.com",
+  password: "admin123",
+  roles: 2
+)
+
+50.times do
+  account = Account.create!(
+    email: Faker::Internet.unique.email,
+    password: "password",
+    roles: 0
+  )
+
+  account.create_user!(
+    full_name: Faker::Name.name,
+    bio: Faker::Lorem.paragraph(sentence_count: 2),
+    goals: Faker::Lorem.paragraph(sentence_count: 2),
+    image_url: "https://i.pinimg.com/1200x/ff/c2/dd/ffc2ddb4e8a9074262ca5192f8d86ee1.jpg"
+  )
+end
